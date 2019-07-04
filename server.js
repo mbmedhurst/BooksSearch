8ecfc9661bf474290c0ee033ee8f41fadd32ff49
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-07-03 17:37:30 -0700

    submit 4

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const { join } = require('path')
 const app = express()
-const PORT = process.env.PORT || 3001
 
 app.use(express.static(join(__dirname, 'client')))
 app.use(express.urlencoded({ extended: true}))
@@ -13,10 +12,6 @@ if (process.env.NODE_ENV === "production") {
 
 require('./routes')(app)
 
-  // when deployed to heroku
-//   var MONGODB_URI = process.env.MONGODB_URI || "mongodb://loclahost/news_db"
-//   mongoose.connect(MONGODB_URI)
-
-require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks_db', { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
-    .then(_ => app.listen(PORT))
+require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://mbmedhurst:marianna1018@ds247377.mlab.com:47377/heroku_2j7zqbv9', { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
+    .then(_ => app.listen(process.env.PORT || 3001))
     .catch(e => console.log(e))
