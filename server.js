ec8e93278b837329a57481564b2094eeaa9f6288
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-07-03 17:24:28 -0700

    submit 3

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const { join } = require('path')
 const app = express()
+const PORT = process.env.PORT || 3001
 
 app.use(express.static(join(__dirname, 'client')))
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('client/build'))
+}
+
 require('./routes')(app)
 
-require('mongoose').connect('mongodb://localhost/googlebooks_db', { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
-    .then(_ => app.listen(3001))
+  // when deployed to heroku
+//   var MONGODB_URI = process.env.MONGODB_URI || "mongodb://loclahost/news_db"
+//   mongoose.connect(MONGODB_URI)
+
+require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks_db', { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
+    .then(_ => app.listen(PORT))
     .catch(e => console.log(e))
