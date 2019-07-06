17ceb6d9275573547ac53bfd9858324930d1c4b8
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-07-06 11:05:56 -0700

    clean tree

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { join } = require('path')
+const path = require('path')
 const app = express()
 
 app.use(express.static(join(__dirname, 'client')))
@@ -10,6 +10,7 @@ if(process.env.NODE_ENV === 'production'){
     //set static folder
     app.use(express.static('client/build'));
 }
+
 app.get('*',(req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 })
