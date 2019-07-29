2211bee927953b55a3c1381a6ae26b4fb644cf48
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-07-28 17:48:31 -0700

    trying to fix map function bug

--- client/src/components/Saved/Card/SavedCard.js
+++ client/src/components/Saved/Card/SavedCard.js
@@ -6,26 +6,26 @@ const SavedCard = props => {
 
     return (
         props.dbBooks.map(({ _id, title, description, authors, thumbnail, infoLink }) => {
-            <>
-            return <Paper style={{ backgroundColor: 'rgba(52,168,83,0.2)', padding: '20px', marginLeft: '50px', marginRight: '50px', marginTop: '50px' }}>
-                <Grid container spacing={3}>
-                    <Grid item xs={6}>
-                        <h2>{title}</h2>
-                        <p style={{fontStyle: 'italic'}}>Written by {authors}</p>
+            return <div>
+                <Paper style={{ backgroundColor: 'rgba(52,168,83,0.2)', padding: '20px', marginLeft: '50px', marginRight: '50px', marginTop: '50px' }}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={6}>
+                            <h2>{title}</h2>
+                            <p style={{ fontStyle: 'italic' }}>Written by {authors}</p>
+                        </Grid>
+                        <Grid item xs={6} style={{ textAlign: 'right' }}>
+                            <button style={{ fontSize: '16px', height: '30px', width: '70px', borderRadius: '4px', backgroundColor: '#4285f4' }} id='view'><a style={{ color: 'white', textDecoration: 'none' }} href={infoLink} target='_blank'>View</a></button>
+                            <button style={{ marginLeft: '5px', fontSize: '16px', height: '30px', width: '70px', borderRadius: '4px', marginLeft: '5px', backgroundColor: '#eb4335', color: 'white' }} value={_id} id='delete' onClick={props.handleDeleteBook}>Delete</button>
+                        </Grid>
+                        <Grid item xs={2} style={{ textAlign: 'center' }}>
+                            <img src={thumbnail}></img>
+                        </Grid>
+                        <Grid item xs={10}>
+                            {description}
+                        </Grid>
                     </Grid>
-                    <Grid item xs={6} style={{textAlign: 'right'}}>
-                        <button style={{ fontSize: '16px', height: '30px', width: '70px', borderRadius: '4px', backgroundColor: '#4285f4' }} id='view'><a style={{ color: 'white', textDecoration: 'none' }} href={infoLink} target='_blank'>View</a></button>
-                        <button style={{ marginLeft: '5px', fontSize: '16px', height: '30px', width: '70px', borderRadius: '4px',marginLeft: '5px', backgroundColor: '#eb4335', color: 'white' }} value={_id} id='delete' onClick={props.handleDeleteBook}>Delete</button>
-                    </Grid>
-                    <Grid item xs={2} style={{textAlign: 'center'}}>
-                        <img src={thumbnail}></img>
-                    </Grid>
-                    <Grid item xs={10}>
-                        {description}
-                    </Grid>
-                </Grid>
-            </Paper>
-            </>
+                </Paper>
+            </div>
         })
     )
 }
