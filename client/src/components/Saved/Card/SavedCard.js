73e580fa6d5f9b18ba13aa2f326e4853c65323b2
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-08-01 17:42:14 -0700

    bug fixing

--- client/src/components/Saved/Card/SavedCard.js
+++ client/src/components/Saved/Card/SavedCard.js
@@ -7,29 +7,31 @@ class SavedCard extends Component {
     render() {
         const { title, _id, description, authors, thumbnail, infoLink } = this.props
 
-        if (this.props.dbBooks.length < 1) {
-            return <div />
-        } return (
-            this.props.dbBooks.map(({ _id, title, description, authors, thumbnail, infoLink }) => {
-                return <Paper style={{ backgroundColor: 'rgba(52,168,83,0.2)', padding: '20px', marginLeft: '50px', marginRight: '50px', marginTop: '50px' }}>
-                    <Grid container spacing={3}>
-                        <Grid item xs={6}>
-                            <h2>{title}</h2>
-                            <p style={{ fontStyle: 'italic' }}>Written by {authors}</p>
-                        </Grid>
-                        <Grid item xs={6} style={{ textAlign: 'right' }}>
-                            <button style={{ fontSize: '16px', height: '30px', width: '70px', borderRadius: '4px', backgroundColor: '#4285f4' }} id='view'><a style={{ color: 'white', textDecoration: 'none' }} href={infoLink} target='_blank'>View</a></button>
-                            <button style={{ marginLeft: '5px', fontSize: '16px', height: '30px', width: '70px', borderRadius: '4px', marginLeft: '5px', backgroundColor: '#eb4335', color: 'white' }} value={_id} id='delete' onClick={this.props.handleDeleteBook}>Delete</button>
-                        </Grid>
-                        <Grid item xs={2} style={{ textAlign: 'center' }}>
-                            <img src={thumbnail}></img>
-                        </Grid>
-                        <Grid item xs={10}>
-                            {description}
-                        </Grid>
+        if (!this.props.dbBooks.length) {
+            return null
+
+        let savedBooks = this.props.dbBooks.map(({ _id, title, description, authors, thumbnail, infoLink }) => {
+            return <Paper style={{ backgroundColor: 'rgba(52,168,83,0.2)', padding: '20px', marginLeft: '50px', marginRight: '50px', marginTop: '50px' }}>
+                <Grid container spacing={3}>
+                    <Grid item xs={6}>
+                        <h2>{title}</h2>
+                        <p style={{ fontStyle: 'italic' }}>Written by {authors}</p>
                     </Grid>
-                </Paper>
-            })
+                    <Grid item xs={6} style={{ textAlign: 'right' }}>
+                        <button style={{ fontSize: '16px', height: '30px', width: '70px', borderRadius: '4px', backgroundColor: '#4285f4' }} id='view'><a style={{ color: 'white', textDecoration: 'none' }} href={infoLink} target='_blank'>View</a></button>
+                        <button style={{ marginLeft: '5px', fontSize: '16px', height: '30px', width: '70px', borderRadius: '4px', marginLeft: '5px', backgroundColor: '#eb4335', color: 'white' }} value={_id} id='delete' onClick={this.props.handleDeleteBook}>Delete</button>
+                    </Grid>
+                    <Grid item xs={2} style={{ textAlign: 'center' }}>
+                        <img src={thumbnail}></img>
+                    </Grid>
+                    <Grid item xs={10}>
+                        {description}
+                    </Grid>
+                </Grid>
+            </Paper>
+        })
+        } return (
+            {savedBooks}  
         )
     }
 }
