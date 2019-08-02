5c38e7c8ea0df350403b7bb0e255dd2188e4cf43
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-08-01 17:02:29 -0700

    bug fixing

--- client/src/components/Saved/Card/SavedCard.js
+++ client/src/components/Saved/Card/SavedCard.js
@@ -7,7 +7,7 @@ class SavedCard extends Component {
     render() {
         const { title, _id, description, authors, thumbnail, infoLink } = this.props
 
-        if (this.props.dbBooks.length < 0) {
+        if (this.props.dbBooks.length < 1) {
             return <div>Loading...</div>
         } else {
             return (
