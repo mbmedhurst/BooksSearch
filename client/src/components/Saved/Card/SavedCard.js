7d8f4df16f28eed704c41f3a89e0983c87e5d4f7
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-08-03 13:11:08 -0700

    clear tree

--- client/src/components/Saved/Card/SavedCard.js
+++ client/src/components/Saved/Card/SavedCard.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react'
 import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
 
-class SavedCard extends Component {
+const SavedCard = ({dbBooks, handleDeleteBook}) => {
+    console.log(dbBooks)
+        // const { dbBooks, title, _id, description, authors, thumbnail, infoLink } = this.props
 
-    render() {
-        const { title, _id, description, authors, thumbnail, infoLink } = this.props
-
-        if (!this.props.dbBooks.length) {
+        if (!dbBooks.length) {
             return <div>Loading...</div>
             // need to somehow get the app to wait for the data to be retunred from the database
         } return (
-            this.props.dbBooks.map(({ _id, title, description, authors, thumbnail, infoLink }) => {
+            dbBooks.map(({ _id, title, description, authors, thumbnail, infoLink }) => {
                 return <Paper style={{ backgroundColor: 'rgba(52,168,83,0.2)', padding: '20px', marginLeft: '50px', marginRight: '50px', marginTop: '50px' }}>
                     <Grid container spacing={3}>
                         <Grid item xs={6}>
@@ -20,7 +19,7 @@ class SavedCard extends Component {
                         </Grid>
                         <Grid item xs={6} style={{ textAlign: 'right' }}>
                             <button style={{ fontSize: '16px', height: '30px', width: '70px', borderRadius: '4px', backgroundColor: '#4285f4' }} id='view'><a style={{ color: 'white', textDecoration: 'none' }} href={infoLink} target='_blank'>View</a></button>
-                            <button style={{ marginLeft: '5px', fontSize: '16px', height: '30px', width: '70px', borderRadius: '4px', marginLeft: '5px', backgroundColor: '#eb4335', color: 'white' }} value={_id} id='delete' onClick={this.props.handleDeleteBook}>Delete</button>
+                            <button style={{ marginLeft: '5px', fontSize: '16px', height: '30px', width: '70px', borderRadius: '4px', marginLeft: '5px', backgroundColor: '#eb4335', color: 'white' }} value={_id} id='delete' onClick={handleDeleteBook}>Delete</button>
                         </Grid>
                         <Grid item xs={2} style={{ textAlign: 'center' }}>
                             <img src={thumbnail}></img>
@@ -33,6 +32,6 @@ class SavedCard extends Component {
             })
         )
     }
-}
+
 
 export default SavedCard
