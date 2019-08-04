6e9f7ab40a0a41d585153dfda73d939633b70987
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-08-03 17:31:26 -0700

    clear tree

--- server.js
+++ server.js
@@ -3,23 +3,16 @@ const bodyParser = require('body-parser')
 const { join, resolve } = require('path')
 const app = express()
 
+let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/adulting_db'
+
 app.use(express.static(join(__dirname, 'client')))
 app.use(express.urlencoded({ extendecd: true }))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
 
-if (process.env.NODE_ENV === 'production') {
-    //set static folder
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
-
-
-
 require('./routes')(app)
+app.get('*', (req, res) => res.sendFile(join(__dirname, 'client', 'build', 'index.html')))
 
-require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks_db', { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true  })
-    .then(_ => app.listen(process.env.PORT || 3001))
-    .catch(e => console.log(e))
+require('mongoose').connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true })
+  .then(_ => app.listen(process.env.PORT || 3001))
+  .catch(e => console.log(e))
