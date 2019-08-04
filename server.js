a5a8492e74892e3312c521561a9c8ef59e2f2320
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-08-03 18:36:28 -0700

    clear tree

--- server.js
+++ server.js
@@ -3,22 +3,14 @@ const bodyParser = require('body-parser')
 const { join, resolve } = require('path')
 const app = express()
 
-app.use(express.static(join(__dirname, 'client')))
+app.use(express.static(join(__dirname, 'client', 'build')))
 app.use(express.urlencoded({ extendecd: true }))
-app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
 
-if (process.env.NODE_ENV === 'production') {
-    //set static folder
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
-
-
-
 require('./routes')(app)
-require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks_db', { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
-    .then(_ => app.listen(process.env.PORT || 3001))
-    .catch(e => console.log(e))
+app.get('*', (req, res) => res.sendFile(join(__dirname, 'client', 'build', 'index.html')))
+
+require('mongoose').connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true })
+  .then(_ => app.listen(process.env.PORT || 3001))
+  .catch(e => console.log(e))
