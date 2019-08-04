f61bf9bc555c8e0241b6410c0c2593fb647fbea8
Montebo <28013382+mbmedhurst@users.noreply.github.com>
2019-08-03 18:39:36 -0700

    clear tree

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const bodyParser = require('body-parser')
 const { join, resolve } = require('path')
 const app = express()
 
+let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks_db'
+
 app.use(express.static(join(__dirname, 'client', 'build')))
 app.use(express.urlencoded({ extendecd: true }))
 // app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json())
+
 
 require('./routes')(app)
 app.get('*', (req, res) => res.sendFile(join(__dirname, 'client', 'build', 'index.html')))
